93db5febda2b2d7e2cd631f25c5ee10cc197bf21
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-10 15:52:20 -0400

    edit click

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 /*if unfixable errors, go to line 1777 in imagemapster*/
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $('#imgmap').mapster({
-            singleSelect: true,
-            fill : true,
-            fillOpacity : 0.6,
-            fillColor: 'ffffff',
-            mapKey: 'data-key',
-            onMouseover: function(e) {
-                $(this).mapster('set',false).mapster('set',true);
-            },
-            onMouseout: function(e) {
-                 $(this).mapster('set',false);
-            }
-        });
+function logtest(x){
+  console.log("Clicked!");
+  console.log(x);
+}
+$('#imgmap').mapster({
+    singleSelect: true,
+    fill : true,
+    fillOpacity : 0.6,
+    fillColor: 'ffffff',
+    mapKey: 'data-key',
+    onMouseover: function(e) {
+        $(this).mapster('set',false).mapster('set',true);
+    },
+    onMouseout: function(e) {
+         $(this).mapster('set',false);
+    }
 });
