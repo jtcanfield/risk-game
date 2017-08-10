651dd0b720693f7857fe831a2ed1de0df49aac03
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-10 17:17:34 -0400

    prep object for insertion

--- main.js
+++ main.js
@@ -15,22 +15,23 @@ $('#imgmap').mapster({
 });
 //okay now dont do anymore jquery
 
-var gameBoardObject = //opens parenting object
-  [//opens array
-    {//opens first object (unnamed) in array
-      "NA1":{//opens object within object within array
+//BEGIN GAMEBOARDOBJECT HOLDER
+var gameBoardObject = [
+    {
+      "NA1":{
       "owner":"red",
       "numberOfTroops":0,
-      }//closes object within object within array
-    },//closes first object (unnamed) in array
-  ];//closes array
+      }
+    },
+];
+
+/*TODO
+STEP ONE: Roll Dice to see who goes first
+STEP TWO: Every Player Puts down a piece
+*/
 
 function logtest(x){
   console.log("Clicked!");
   console.log(x);
   console.log(gameBoardObject);
 }
-/*TODO
-STEP ONE: Roll Dice to see who goes first
-STEP TWO: Every Player Puts down a piece
-*/
