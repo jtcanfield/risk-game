6ddbde247138fd11a01567b522bafb6c455c07ce
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-10 14:58:26 -0400

    add working higlights

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-$('#Map').mapster();
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('#imgmap').mapster({
+            singleSelect: true,
+            fill : true,
+            fillOpacity : 0.6,
+            fillColor: 'ffffff',
+            onMouseover: function(e) {
+                $(this).mapster('set',false).mapster('set',true);
+            },
+            onMouseout: function(e) {
+                 $(this).mapster('set',false);
+            }
+        });
+});
