ca50e2139c20e29c09fd33c6eaaa859f155c756e
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-10 17:15:55 -0400

    add object for game board

--- main.js
+++ main.js
@@ -15,9 +15,22 @@ $('#imgmap').mapster({
 });
 //okay now dont do anymore jquery
 
+var gameBoardObject = //opens parenting object
+  [//opens array
+    {//opens first object (unnamed) in array
+      "NA1":{//opens object within object within array
+      "owner":"red",
+      "numberOfTroops":0,
+      }//closes object within object within array
+    },//closes first object (unnamed) in array
+  ];//closes array
 
 function logtest(x){
   console.log("Clicked!");
   console.log(x);
+  console.log(gameBoardObject);
 }
-// STEP ONE: Every Player Puts down a piece
+/*TODO
+STEP ONE: Roll Dice to see who goes first
+STEP TWO: Every Player Puts down a piece
+*/
